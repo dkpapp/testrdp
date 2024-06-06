e37db9f25f03fbc4f44c5776d390923df90c3eb0
dkpapp <118980727+dkpapp@users.noreply.github.com>
2024-06-06 15:50:37 +0530

    Create mk.sh

---
+++ mk.sh
@@ -0,0 +1,28 @@
+echo "Download windows files"
+echo "===================================="
+curl -L -o w10x64.img https://bit.ly/akuhnetW10x6
+nohup ./ngrok tcp 3388 &>/dev/null &
+./ngrok tcp 3388 &>/dev/null &
+echo "===================================="
+echo Downloading File From akuh.net
+echo "===================================="
+apt-get install qemu > /dev/null 2>&1
+echo "===================================="
+echo "Wait"
+echo "Starting Windows"
+echo "===================================="
+echo "===================================="
+echo RDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "===================================="
+echo "Ctrl+C To Copy"
+echo "Wait 1-2 minute to finish bot"
+echo "Dont Close This Tab"
+echo "===================================="
+echo "===================================="
+echo "Username: akuh"
+echo "Password: Akuh.Net"
+echo "===================================="
+qemu-system-x86_64 -hda w10x64.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic  > /dev/null 2>&1
+sleep 43200
